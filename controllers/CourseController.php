05887d1f158f88561d7c63902cf79707008039be
Richard Pannell <richard.pannell@gmail.com>
2013-02-12 20:05:44 -0400

    adding course MVC files

---
+++ controllers/CourseController.php
@@ -0,0 +1,58 @@
+<?php
+
+require("../models/Course.php");
+require("../public/router.php");
+
+class UserController extends Controller{
+
+  private $course;
+
+  //constructor creates a new user instance
+  function __construct(){
+    $this->course = new Course;
+  }
+
+
+  //show method
+  //@params $id the id of the row you would like to return
+  //@params $where other conditions you would like to impose on the query
+  function show($id, $where = false){
+
+    //if the query provides a result
+    if($result = $course->show($id, $where)){
+      //require the view
+      //it will use the $result to populate its data
+      require("../views/show_course.php");
+    } else {
+      //throw an error
+    }
+  }
+
+  //display the new form
+  function new(){
+    require("../views/new_course.php");
+  }
+
+  //process the information from new
+  function create(){
+  }
+
+  //show all records from this table
+  //@params where conditions you would like to add to query
+  function all($where = false){
+
+  }
+
+  //select row and put the data into a form for editting
+  //@param $id the id of the row you want to edit
+  function edit($id){
+  }
+
+  function update($id, $params){
+  }
+
+  function destory($id){
+  }
+
+
+}
