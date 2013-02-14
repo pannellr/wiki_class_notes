4b71ed06b2d03e6f2716995718aaa614ecc818eb
Richard Pannell <richard.pannell@gmail.com>
2013-02-13 20:48:23 -0400

    Addint UnitTest class for testing and Error Class for Errors

---
+++ tests/UnitTest.php
@@ -0,0 +1,20 @@
+<?php
+
+class UnitTest{
+
+  public test;
+
+  function __construct(){
+    $this->test = new UnitTest;
+  }
+
+  function assertEquals(){
+  }
+
+  function assertTrue(){
+  }
+
+  function assertFalse(){
+  }
+
+}
