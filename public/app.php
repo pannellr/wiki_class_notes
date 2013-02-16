0f4f91aa9d09bffeec559daceb62b77051f9bd2d
Richard Pannell <richard.pannell@gmail.com>
2013-02-16 18:06:30 -0400

    routing finally working

--- public/app.php
+++ public/app.php
@@ -1,10 +1,39 @@
 <?php
-require("../controllers/controllers.php");   
-$page = $_GET['page'];
 
-$action = explode('_', $page);
+//Reqire all of the controllers
+require("../controllers/controllers.php");
 
-$controller = ucfirst($action[1]) . "Controller";
+//.htaccess passes the class name through a GET request
+//make the first letter uppercase and store it in controller   
+$controller = ucfirst($_GET['class']) . 'Controller';
 
+//.htaccess passes the method through a GET request
+//it is in the not_camel_case style
+//use toCamelCaseMethod below 
+$method = toCamelCase($_GET['method']);
+
+//Remove the class and the method indexes
+//so the remaining GET params canned be passed by the method
+unset($_GET['class']);
+unset($_GET['method']);
+
+//Create a controller instance
 $app = new $controller();
-$app->$action[0]();
+
+//Call the method passing the parameters 
+//that remain in the GET request
+$app->$method($_GET);
+
+//Untility to convert underscore names
+//to camelCase
+function toCamelCase($string){
+  $strings = explode("_", $string);
+  
+  $camelCased = lcfirst($strings[0]);
+  //start at 1, skipping the first string
+  for ($i = 1; $i < count($stings); $i++){
+    $camelCased .= $stings[$i];
+  }
+
+  return $camelCased;
+}
