0beebe42af53bd8b2e71a93c1fe9bf2b3fd1506b
Richard Pannell <richard.pannell@gmail.com>
2013-02-28 19:49:41 -0400

    updates from earlier

--- controllers/Flash.php
+++ controllers/Flash.php
@@ -1,18 +1,18 @@
 <?php
 
-	public class Flash(){
-		//instance variables
-		private $type;
-		private $message
-
-		function __construct($message, $type){
-			$this->message = $message;
-			$this->$type = $type;
-		}
-
-		public function display(){
-			echo "<div class=\"flash ".$this->type."\">".$this->message."</div>";
-		}
-
-
-	}
+public class Flash(){
+  //instance variables
+  private $type;
+  private $message;
+    
+    function __construct($message, $type){
+    $this->message = $message;
+    $this->$type = $type;
+  }
+  
+  public function display(){
+    echo "<div class=\"flash ".$this->type."\">".$this->message."</div>";
+  }
+  
+  
+}
