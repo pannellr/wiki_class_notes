fb33bca11973a2ef3ac987ada62d662b687f2f69
viviankou <vivianxinxinkou@gmail.com>
2013-03-01 14:42:28 -0400

    abc

---
+++ config/Config.php
@@ -0,0 +1,34 @@
+<?php
+
+class Config{
+	
+	private $host;
+	private $username;
+	private $password;
+	private $DBname;
+	
+	function __construct(){
+	 $this->host="localhost";
+	 $this->username="root";
+	 $this->password="";
+	 $this->DBname="sdugas_esarve";
+	}
+	
+	function getHost(){
+	return $this->host;
+	}
+	
+	function getUsername(){
+	return $this->username;
+	}
+	
+	function getPassword(){
+	return $this->password;
+	}
+	
+	function getDBname(){
+	return $this->DBname;
+	}
+	
+	}
+	
