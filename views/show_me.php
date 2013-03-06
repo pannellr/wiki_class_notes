99918e4e3978f395c7cd60794cc5ebe197845d58
simdugas <simdugas@gmail.com>
2013-03-06 11:37:03 -0400

    added login functionality

--- views/show_me.php
+++ views/show_me.php
@@ -1,11 +1,13 @@
-<h3>Welcome <?php echo($data['user_name']); ?>!</h3>
+<h3>Welcome <?php echo($data['user']['user_name']); ?>!</h3>
 
 <?php
 echo "<table><tr><td>user_name</td><td>created on</td><td>person id</td></td><td>edit</td><td>delete</td></tr>";
-echo "<tr><td>".$data['id']."</td><td>" . $data['user_name'] . "</td>";
-echo "<td>".$data['created_at']."</td>";
-//echo "<td><a href=\"edit?id=" . $data['id'] . "\">Edit</a></td>"
-. "<td><a href=\"destroy?id=" . $data['id'] . "\">Delete</a></td></tr>";
-//echo "</table>";
+echo "<tr><td>".$data['user']['id']."</td><td>" . $data['user']['user_name'] . "</td>";
+echo "<td>".$data['user']['created_at']."</td>";
+echo "<td><a href=\"edit?id=" . $data['user']['id'] . "\">Edit</a></td>"
+. "<td><a href=\"destroy?id=" . $data['user']['id'] . "\">Delete</a></td></tr>";
+echo "</table>";
+echo "<pre>";
+echo "</pre>";
 
 ?>
