84c875e3e57a71ea945bc1de13786e7701cba3fb
simdugas <simdugas@gmail.com>
2013-03-17 14:56:25 -0300

    changed isset

--- views/show_me.php
+++ views/show_me.php
@@ -1,13 +1,18 @@
-<h3>Welcome <?php echo($data['user']['user_name']); ?>!</h3>
+<?php if(!empty($user)) { ?>
+<h3>Welcome <?php echo($user['user_name']); ?>!</h3>
 
 <?php
 echo "<table><tr><td>user_name</td><td>created on</td><td>person id</td></td><td>edit</td><td>delete</td></tr>";
 
-echo "<tr><td>".$data['user']['id']."</td><td>" . $data['user']['user_name'] . "</td>";
-echo "<td>".$data['user']['created_at']."</td>";
-echo "<td><a href=\"edit?id=" . $data['user']['id'] . "\">Edit</a></td>";
-echo "<td><a href=\"destroy?id=" . $data['user']['id'] . "\">Delete</a></td></tr>";
+echo "<tr><td>".$user['id']."</td><td>" . $user['user_name'] . "</td>";
+echo "<td>".$user['created_at']."</td>";
+echo "<td><a href=\"edit?id=" . $user['id'] . "\">Edit</a></td>";
+echo "<td><a href=\"destroy?id=" . $user['id'] . "\">Delete</a></td></tr>";
 echo "</table>";
+
+} else { ?>
+	<h3>You are not logged in!</h3>
+<?php }  
 // echo "<pre>";
 // echo "</pre>";
 // echo "<tr><td>".$data['id']."</td><td>" . $data['user_name'] . "</td>";
