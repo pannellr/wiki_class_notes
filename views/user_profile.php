e7647a323bee5caac9e4b4dad106ed7aeaa3f84a
simdugas <simdugas@gmail.com>
2013-03-18 13:44:22 -0300

    made courses modular

--- views/user_profile.php
+++ views/user_profile.php
@@ -5,17 +5,6 @@
 
 	<h2>Welcome <?php echo $data['user']['user_name']; ?></h2>
 
-	<h3>Your Courses</h3>
-		<?php if(!empty($data['courses'])) { ?>
-		<ul id="course-list" class="courses">
-			<?php 
-				foreach ($data['courses'] as $key => $value) { ?>
-				<li><?php echo $value['name']; ?></li>
-			<?php } ?>
-		</ul>
-		<?php } else { ?>
-			<p>You are not subscribed to any courses.</p>
-		<?php } ?>
-	<a href="/student/add_course?person_id=<?php echo $data['user_info']['person_id'] ?>">Join a course</a>
-	<a href="/course/fresh">Create a course</a>
+	<?php require_once("student_classes.php"); ?>
+	
 <?php } ?>
