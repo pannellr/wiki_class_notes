10d04205e87b7d3e8d203266b780f9a0595b2967
simdugas <simdugas@gmail.com>
2013-03-18 14:16:31 -0300

    changed available courses to not show courses already subscribed to.

--- models/Student.php
+++ models/Student.php
@@ -40,14 +40,48 @@ SQL;
 
   public function availableCourses(){
     
-    $query =<<<SQL
-      select c.name
-      , c.number
-      , s.id as section_id
-      , s.section_number
-from courses c
-join sections s
-  on s.course_id = c.id;
+//     $query =<<<SQL
+//  select c.id as course_id,
+//     se.id as section_id, 
+//     d.shortname,
+//     c.number,
+//     c.name,
+//     se.section_number 
+// from sections se
+// join courses c
+//   on c.id = se.course_id
+// join departments d
+//   on d.id=c.department_id;
+// SQL;
+
+    //wont show courses that the student is already in
+$query = <<<SQL
+    select c.id as course_id,
+    se.id as section_id, 
+    d.shortname,
+    c.number,
+    c.name,
+    se.section_number 
+from sections se
+join courses c
+  on c.id = se.course_id
+join departments d
+  on d.id=c.department_id
+WHERE course_id NOT IN(
+select c.id as course_id
+from students s
+join people p
+  on p.id = s.person_id
+join sections se
+  on se.id = s.section_id
+join courses c
+  on c.id = se.course_id
+join users u 
+  on u.person_id = p.id
+join departments d
+  on d.id=c.department_id
+where u.id = 39
+);
 SQL;
 
     $classes = $this->query($query);
