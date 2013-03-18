363e787fb350e4dd61fc6acec58ace4990d7ff26
simdugas <simdugas@gmail.com>
2013-03-18 10:40:26 -0300

    A user profile page

---
+++ views/user_profile.php
@@ -0,0 +1,19 @@
+<?php print_r($data) ?>
+<?php if(!empty($data['user'])) { ?>
+
+	<h2>Welcome <?php $data['user']['user_name'] ?></h2>
+
+	<h3>Your Courses</h3>
+		<?php if(!empty($data['courses'])) { ?>
+		<ul id="course-list" class="courses">
+			<?php 
+				foreach ($data['course'] as $key => $value) { ?>
+				<li><?php echo $key; ?></li>
+			<?php } ?>
+		</ul>
+		<?php } else { ?>
+			<p>You are not subscribed to any courses.</p>
+		<?php } ?>
+	<a href="student/add_class?person_id=<?php echo $data['user']['person_id'] ?>">Join a course</a>
+	<a href="course/fresh">Create a course</a>
+<?php } ?>
