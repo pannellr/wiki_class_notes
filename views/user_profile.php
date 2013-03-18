b28d365a9425be8a6235a02b75a77c1c09cff8da
simdugas <simdugas@gmail.com>
2013-03-18 11:12:58 -0300

    added userdata

--- views/user_profile.php
+++ views/user_profile.php
@@ -1,7 +1,9 @@
+<pre>
 <?php print_r($data) ?>
+</pre>
 <?php if(!empty($data['user'])) { ?>
 
-	<h2>Welcome <?php $data['user']['user_name'] ?></h2>
+	<h2>Welcome <?php echo $data['user']['user_name']; ?></h2>
 
 	<h3>Your Courses</h3>
 		<?php if(!empty($data['courses'])) { ?>
@@ -14,6 +16,6 @@
 		<?php } else { ?>
 			<p>You are not subscribed to any courses.</p>
 		<?php } ?>
-	<a href="student/add_class?person_id=<?php echo $data['user']['person_id'] ?>">Join a course</a>
-	<a href="course/fresh">Create a course</a>
+	<a href="/student/add_class?person_id=<?php echo $data['user_info']['person_id'] ?>">Join a course</a>
+	<a href="/course/fresh">Create a course</a>
 <?php } ?>
