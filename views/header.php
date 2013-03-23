c817164473adc4b389ca06196a52dcc2daa3e41f
Richard Pannell <richard.pannell@gmail.com>
2013-03-23 11:09:52 -0300

    adding image, removing navbar

--- views/header.php
+++ views/header.php
@@ -21,4 +21,11 @@
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
    <?php require_once("session_info.php"); ?>
-   <?php require_once('navbar.php'); ?>
+   
+   <header class="banner">
+
+   <img src="/img/PaperPlane.png" width="215" height="120" >
+
+   </header>
+
+   <?php //require_once('navbar.php'); ?>
